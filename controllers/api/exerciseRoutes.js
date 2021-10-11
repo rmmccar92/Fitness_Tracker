eccfd57c9f219d33c43a0750819556ad07665e21
Ryan McCarthy <rmmcc92@aol.com>
2021-10-11 14:38:30 -0400

    added api routes they seem to be broken though

--- controllers/api/exerciseRoutes.js
+++ controllers/api/exerciseRoutes.js
@@ -1,3 +1,51 @@
-const router = require("express").Router();
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+const Workout = require("../../models/Workout");
+
+router.get("api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("api/workouts", ({ body }, res) => {
+  db.Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("api/workouts/:id", ({ params, body }, res) => {
+  db.Workout.findOneAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
---
+++ models/Workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+
+      duration: {
+        type: String,
+        required: true,
+      },
+
+      weight: {
+        type: Number,
+      },
+
+      reps: {
+        type: Number,
+      },
+
+      sets: {
+        type: Number,
+      },
+
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+module.exports = Workout;
